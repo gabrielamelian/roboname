32f4264e53784fe199651cf04a6ebb9bc56452fb
gabrielamelian <gabrielamelian@gmail.com>
2016-10-13 11:11:52 +1300

    Moved scripts to bottom of body and other changes on js file that did not work

--- roboname.js
+++ roboname.js
@@ -1,25 +1,30 @@
 $(document).ready(function () {
 
     function getRobot() {
-    $.ajax({
-       url: 'https://robohash.p.mashape.com/index.php',
-       data: {
-          format: 'json'
-       },
-       error: function() {
-          $('#board').html('<p>An error has occurred</p>');
-       },
-       dataType: 'json',
-       success: function(data) {
-          var roboObject = data;
-          var $robot = $('<img src=">').text(roboObject.imageURL);
-          $('#1')
-             .replaceWith($quote + '">');
-       },
-       beforeSend: function(xhr) {
-          xhr.setRequestHeader("X-Mashape-Authorization", "NWp9T0eaFEmsh8glExwzYkerHQrCp1jNYEajsnCdl7zVX7LXVG");
-       }
-    });
-}
+        $.ajax({
+           url: 'https://robohash.p.mashape.com/index.php',
+           data: {
+              format: 'json'
+           },
+           error: function() {
+                console.log("an error has occured")
+                $('#board').html('<p>An error has occurred</p>');
+           },
+           dataType: 'json',
+           success: function(roboObject) {
+              console.log(roboObject);
+              var robot = roboObject.imageUrl;
+              console.log(robot);
+              return robot;
+           },
+           beforeSend: function(xhr) {
+              xhr.setRequestHeader("X-Mashape-Authorization", "NWp9T0eaFEmsh8glExwzYkerHQrCp1jNYEajsnCdl7zVX7LXVG");
+           }
+        });
+    }
+
+    for (var i = 0; i < 31; i++) {
+        $('#' + i).replaceWith('<img src="' + getRobot() + '">');
+    }
 
 });
