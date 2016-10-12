f0e0003ed90be8e30642ea65fc26deb4b88db25d
gabrielamelian <gabrielamelian@gmail.com>
2016-10-13 11:22:18 +1300

    Added function to enter random robots up to 30

--- roboname.js
+++ roboname.js
@@ -1,27 +1,11 @@
 $(document).ready(function () {
 
     function getRobot() {
-        $.ajax({
-           url: 'https://robohash.p.mashape.com/index.php',
-           data: {
-              format: 'json'
-           },
-           error: function() {
-                console.log("an error has occured")
-                $('#board').html('<p>An error has occurred</p>');
-           },
-           dataType: 'json',
-           success: function(roboObject) {
-              console.log(roboObject);
-              var robot = roboObject.imageUrl;
-              console.log(robot);
-              return robot;
-           },
-           beforeSend: function(xhr) {
-              xhr.setRequestHeader("X-Mashape-Authorization", "NWp9T0eaFEmsh8glExwzYkerHQrCp1jNYEajsnCdl7zVX7LXVG");
-           }
-        });
+        var randomHash = Math.random().toString(36).substring(7);
+        return "https://robohash.org/" + randomHash;
     }
+
+    console.log(getRobot());
 
     for (var i = 0; i < 31; i++) {
         $('#' + i).replaceWith('<img src="' + getRobot() + '">');
