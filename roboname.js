b20fae388eac180a149615e5423a7cd8c7349bfb
gabrielamelian <gabrielamelian@gmail.com>
2016-10-13 16:49:04 +1300

    added function to generate blank board

--- roboname.js
+++ roboname.js
@@ -1,26 +1,30 @@
 $(document).ready(function () {
 
+    var board = {
+        cells: []
+    }
+
+    startGame();
+
     function getRobot() {
         var randomHash = Math.random().toString(36).substring(7);
         return "https://robohash.org/" + randomHash;
-    }
-
-    var board = {
-        cells: []
     }
 
     function generateBoard () {
         for (var i = 0; i < 20; i++) {
             board.cells.push({
                 roboNumber: i,
-                hiddenImage: '<img src="binary.jpg">',
-                robotImage: getRobot(),
+                hiddenImage: "binary.jpg",
+                robotImage: getRobot()
             })
         }
     }
 
-    for (var i = 0; i < 20; i++) {
-        $('#board').append('<img src="' + getRobot() + '">');
+    function startGame () {
+        generateBoard();
+        for (var i = 0; i < 20; i++) {
+            $('#board').append('<img src="' + board.cells[i].hiddenImage + '">');
+        }
     }
-
 });
