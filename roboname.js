b219a7966cc800fc4e262dcedb4f2be9a71aa3aa
gabrielamelian <gabrielamelian@gmail.com>
2016-10-13 21:49:59 +1300

    Moved update DOM to a different function, created all functions as global values

--- roboname.js
+++ roboname.js
@@ -1,50 +1,56 @@
-$(document).ready(function () {
-    //Initialize board object
-    var board = {
-        cells: []
-    }
+$(document).ready(startGame());
 
-    //Call to populate DOM
-    startGame();
-    preload(board.cells);
+//Initialize board object
+var board = {
+    cells: []
+}
 
-    //Initializes the game
-    function startGame () {
-        generateBoard();
-        for (var i = 0; i < 20; i++) {
-            $('#board').append('<img id="' + i + '" src="' + board.cells[i].hiddenImage + '">');
-        }
-        $('#board').on('click', 'img', showRobot);
-    }
+//Call to populate DOM
+startGame();
+preload(board.cells);
 
-    //Populates the board object
-    function generateBoard () {
-        for (var i = 0; i < 20; i++) {
-            board.cells.push({
-                roboNumber: i,
-                hiddenImage: "binary.jpg",
-                robotImage: getRobot()
-            })
-        }
-    }
+//Initializes the game
+function startGame () {
+    generateBoard();
+    populateDOM();
+    $('#board').on('click', 'img', showRobot);
+}
 
-    //Generates a random robot URL
-    function getRobot() {
-        var randomHash = Math.random().toString(36).substring(7);
-        return "https://robohash.org/" + randomHash;
+//Populates the board object
+function generateBoard () {
+    for (var i = 0; i < 20; i++) {
+        board.cells.push({
+            roboNumber: i,
+            hiddenImage: "binary.jpg",
+            robotImage: getRobot()
+        })
     }
+}
 
-    //Pre-load images
-    function preload(arrayOfImages) {
-        $(arrayOfImages).each(function(){
-            $('<img/>').attr('src',this.robotImage);
-        });
+//Add board elements on DOM
+function populateDOM() {
+    $('#board').empty();
+    for (var i = 0; i < 20; i++) {
+        $('#board').append('<img id="' + i + '" src="' + board.cells[i].hiddenImage + '">');
     }
+}
 
-    //Toggle image to show robot
-    function showRobot() {
-        var buttonId = $(this).attr('id');
-        var robot = board.cells.filter(function(element) { return buttonId == element.roboNumber });
-        $('#' + buttonId).attr('src',robot[0].robotImage);
-    }
-});
+//Generates a random robot URL
+function getRobot() {
+    var randomHash = Math.random().toString(36).substring(7);
+    return "https://robohash.org/" + randomHash;
+}
+
+//Pre-load images
+function preload(arrayOfImages) {
+    $(arrayOfImages).each(function(){
+        $('<img/>').attr('src',this.robotImage);
+    });
+}
+
+//Toggle image to show robot
+function showRobot() {
+    var buttonId = $(this).attr('id');
+    var robot = board.cells.filter(function(element) { return buttonId == element.roboNumber });
+    $('#' + buttonId).attr('src',robot[0].robotImage);
+}
